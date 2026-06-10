40f6ef1b133e12ea2afca1e8991aff3357b3342c
Uvay01 <ST10477087@myemeris.edu.za>
2026-06-10 12:14:34 +0200

    Second commit for screen 1 code done

--- app/src/main/java/com/example/inventoryapp/MainActivity.kt
+++ app/src/main/java/com/example/inventoryapp/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.inventoryapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,12 +19,23 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-
-
-
-
-
         }
+    }
+}
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("SplashActivity", "Splash screen loaded")
+
+        // Transition to the Main Screen after 3000ms
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Prevents the user from going back to the splash screen
+            Log.d("SplashActivity", "Navigating to MainActivity")
+        }, 3000)
     }
 }
